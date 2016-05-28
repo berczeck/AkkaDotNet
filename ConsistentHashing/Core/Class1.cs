3b27268ed210821572ddd1ed5cf276e0eb85efce
Luis Alexander Aldazabal Gil <alexaldazabal@outlook.com>
2016-05-28 02:58:25 +0000

    consistent hash

---
+++ ConsistentHashing/Core/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Persistence;
+
+namespace Core
+{
+    public interface IHasCustomKey
+    {
+        string Identifier { get; }
+    }
+    public class CustomerActor : ReceivePersistentActor, IHasCustomKey
+    {
+        public string Identifier
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string PersistenceId { get; }
+
+        public CustomerActor()
+        {
+            PersistenceId = $"{Context.Parent.Path.Name}-{Self.Path.Name}";
+        }
+
+    }
+}
---
+++ ConsistentHashing/Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Routing;
+using Core;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var system = ActorSystem.Create("ConsistentHashingSystem"))
+            {
+                var router = system.ActorOf(Props.Create<CustomerActor>().WithRouter(new ConsistentHashingPool(5).WithHashMapping(o =>
+{
+    if (o is IHasCustomKey)
+        return ((IHasCustomKey)o).Identifier;
+
+    return null;
+})));
+                Console.WriteLine("PersistenceActormSystem started...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
